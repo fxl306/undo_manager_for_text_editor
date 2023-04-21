69c6eaee4a56f0eec9c87e0f41697373be2532ca
fxl306 <99989424+fxl306@users.noreply.github.com>
2023-04-21 17:39:50 -0400

    update UndoManagerRepTesterWithMockito.java

--- src/test/java/UndoManagerRepTesterWithMockito.java
+++ src/test/java/UndoManagerRepTesterWithMockito.java
@@ -1,7 +1,18 @@
+
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+/**
+ * This class represents the testing class for the UndoManagerRep class.
+ * Mockito is used here to mock the Change api
+ * @auther Feng Long (fxl306@case.edu)
+ * @Date 04/21/2023
+ */
+
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import undo.impl.*;
 import undo.api.*;
 
@@ -13,9 +24,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UndoManagerRepTesterWithMockito {
     @Mock
     Change mockChange;
-
-     //@Mock
-     //ChangeFactory mockChangeFactory;
 
     DocumentRep doc;
 
@@ -30,7 +38,8 @@ public class UndoManagerRepTesterWithMockito {
 
     @Test
     public void registerChangeTest(){
-        // when(mockChangeFactory.createInsertion(0, "NewAdd", 0, 5)).return(mockChange);
+
+        // mock a virtual Change operation as the ChangeImpl.apply() is not implemented.
         doNothing().when(mockChange).apply(doc);
 
         undoManagerRep.registerChange(mockChange);
@@ -40,6 +49,33 @@ public class UndoManagerRepTesterWithMockito {
         assertTrue(undoManagerRep.getLatestChangeIndex() == 0);
 
         verify(mockChange).apply(doc);
+    }
+
+    @Test
+    public void undoInsertionTest() {
+
+        // mock a virtual insertionChange operation as the ChangeImpl.apply() is not implemented.
+        doAnswer(new Answer<Document>() {
+            public Document answer(InvocationOnMock invocation) {
+                doc.insert(0, "NewAdd");
+                return doc;
+            }
+        }).when(mockChange).apply(doc);
+        undoManagerRep.registerChange(mockChange); // mock the insertionChange operation
+        verify(mockChange).apply(doc);
+        assertEquals("NewAddHello world", doc.getDocText().toString()); // verify successful insertionChange
+
+        // mock a virtual undo operation as the ChangeImp.revert() is not implemented.
+        doAnswer(new Answer<Document>() {
+            public Document answer(InvocationOnMock invocation) {
+                doc.delete(6, "NewAdd");
+                return doc;
+            }
+        }).when(undoManagerRep.getOperations().get(undoManagerRep.getLatestChangeIndex())).revert(doc);
+
+        // Perform undo previous operation(insertionChange)
+        undoManagerRep.undo();
+        assertEquals("Hello world", doc.getDocText().toString());
     }
 
 
